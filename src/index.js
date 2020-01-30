89ec244387bda0cd9471f636d82ae6608a69173d
Anton Floretsky <afloretsky@gmail.com>
2020-01-30 13:50:48 +0300

    Optimized returnCounter function

--- src/index.js
+++ src/index.js
@@ -60,9 +60,7 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 13
  */
 function returnCounter(number = 0) {
-    return function F() {
-        return ++number;
-    }
+    return () => ++number;
 }
 
 /*
